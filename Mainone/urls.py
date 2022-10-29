099d0c00878e9c99f1442beb0c194d2a444e0499
Codeburst09 <meher.madhavareddy@gmail.com>
2022-10-29 11:54:34 +0530

    four commit

--- Mainone/urls.py
+++ Mainone/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('safensecure.Urls')),
 
-     
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
