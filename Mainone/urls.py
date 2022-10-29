8848b338e0381006fc2ba55a203ba02fcf4da174
Codeburst09 <meher.madhavareddy@gmail.com>
2022-10-29 19:48:29 +0530

    four commit

--- Mainone/urls.py
+++ Mainone/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from django.views.static import serve
 
-from Mainone import settings
+from MAINONE import settings
 
 
 urlpatterns = [
